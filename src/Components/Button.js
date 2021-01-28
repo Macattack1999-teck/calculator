2a76b2d360bc9e708374d7cd99e0b7644768fc5f
Michael Cortez <CortezDevelopement@gmail.com>
2021-01-27 21:57:27 -0700

    now not allowing a user to add multiple operators when one is already in use, for example if a user has 6+ and then trys to add another + the operation does not change. however if the user has 6+ and then trys to use a different operator (ex -, /, *) then the operation changes to 6(new operator)

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import OperationContext from '../Contexts/OperationContext';
 
 export default (props) => {
-  const buttonName = props.button === "/" ? "slash"
-     : props.button === "ac" ? "ac"
-     : props.button === "-" ? "dash" 
-     : props.button === "+" ? "plus" 
-     : props.button === "=" ? "equals" 
-     : props.button === "." ? "dot" 
-     : props.button === "1" ? "one"
-     : props.button === "2" ? "two"
-     : props.button === "3" ? "three"
-     : props.button === "4" ? "four"
-     : props.button === "5" ? "five"
-     : props.button === "6" ? "six"
-     : props.button === "7" ? "seven"
-     : props.button === "8" ? "eight"
-     : props.button === "9" ? "nine"
-     : props.button === "0" ? "zero"
-     : props.button === "x" ? "times"
-     : props.button
+  const {
+    setOperation,
+    operation
+  } = useContext(OperationContext);
 
-     const handleAddingToOperation = () => {
-      console.log(props.button)
-     }
+  const buttonName = props.button === "/" ? "slash"
+    : props.button === "ac" ? "ac"
+    : props.button === "-" ? "dash" 
+    : props.button === "+" ? "plus" 
+    : props.button === "=" ? "equals" 
+    : props.button === "." ? "dot" 
+    : props.button === "1" ? "one"
+    : props.button === "2" ? "two"
+    : props.button === "3" ? "three"
+    : props.button === "4" ? "four"
+    : props.button === "5" ? "five"
+    : props.button === "6" ? "six"
+    : props.button === "7" ? "seven"
+    : props.button === "8" ? "eight"
+    : props.button === "9" ? "nine"
+    : props.button === "0" ? "zero"
+    : props.button === "x" ? "times"
+    : props.button
+
+    const handleAddingToOperation = () => {
+      if (props.button === "ac") {
+        setOperation("")
+      } else if (props.button === "=") {
+        if (operation.length > 0) {
+          
+        }
+      } else {
+        if (props.button === "+" || props.button === "-" || props.button === "x" || props.button === '/') {
+          if (props.button === operation[operation.length - 1]) {
+            return
+          } else if (operation[operation.length - 1] === "+" || operation[operation.length - 1] === "-" || operation[operation.length - 1] === "x" || operation[operation.length - 1] === "/") {
+            const slicedOperation = operation.slice(0, -1)
+            const operator = props.button === "x" ? "*" : props.button
+            const completeOperation = slicedOperation + operator
+            return setOperation(completeOperation)
+          }
+        }
+
+        if (props.button === "x") {
+          setOperation(operation + "*")
+        } else {
+          setOperation(operation + props.button)
+        }
+      }
+    }
 
   return (
     <div onClick={handleAddingToOperation} className={`button ${buttonName}`} style={{ gridArea: buttonName }}>
--- src/Providers/OperationProvider.js
+++ src/Providers/OperationProvider.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import OperationContext from '../Contexts/OperationContext';
 
 const OperationProvider = (props) => {
-  const [operation, setOperation] = useState('9+9/2');
+  const [operation, setOperation] = useState("");
 
   const state = {
     operation,
