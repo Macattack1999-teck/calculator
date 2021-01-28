9ff0941239f1dbcb32ce3992e8ed8a5bbfee6efc
Michael Cortez <CortezDevelopement@gmail.com>
2021-01-28 02:16:14 -0700

    got rid of errors in App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import { useContext } from 'react'
-import OperationContext from './Contexts/OperationContext';
-import ButtonsContainer from './Components/ButtonsContainer';
 import CalculatorContainer from './Components/CalculatorContainer';
 
 function App() {
-  const {
-    operation
-  } = useContext(OperationContext)
-
   return (
     <div className="App" style={{ 
         background: "#1d1d1d", height: "100%", width: "100%", minHeight: "100vh", minWidth: "100vw",
--- src/Components/Display/DisplayTop.js
+++ src/Components/Display/DisplayTop.js
@@ -3,12 +3,29 @@ import OperationContext from '../../Contexts/OperationContext';
 
 export default () => {
   const {
-    operation
+    operation,
+    sum
   } = useContext(OperationContext)
 
   return (
-    <div>
-      {operation}
+    <div style={{ display: "flex" }}>
+      <div>
+        {operation}
+      </div>
+
+      {
+        sum ? (
+          <div style={{ display: "flex" }}>
+            <div>
+              =
+            </div>
+
+            <div>
+              {sum}
+            </div>
+          </div>
+        ) : null
+      }
     </div>
   )
 }
